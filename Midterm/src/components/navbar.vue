a46ed0bb3eedbc2d8c214993ad09f955d53085df
fennerii <jonathanfennerii@gmail.com>
2026-03-16 02:07:58 -0400

    added photo to navbar

--- Midterm/src/components/navbar.vue
+++ Midterm/src/components/navbar.vue
@@ -5,7 +5,6 @@
         HeavyLifting
       </span>
     </div>
-
     <div class="navbar-menu">
       <div class="navbar-start">
         <RouterLink to="/dashboard" class="navbar-item">
@@ -14,43 +13,45 @@
             <span>Dashboard</span>
           </span>
         </RouterLink>
-
         <RouterLink to="/activities" class="navbar-item">
           <span class="icon-text">
             <span class="icon"><i class="fas fa-running"></i></span>
             <span>Activities</span>
           </span>
         </RouterLink>
-
         <RouterLink to="/stats" class="navbar-item">
           <span class="icon-text">
             <span class="icon"><i class="fas fa-chart-bar"></i></span>
             <span>Stats</span>
           </span>
         </RouterLink>
-
         <RouterLink to="/friends" class="navbar-item">
           <span class="icon-text">
             <span class="icon"><i class="fas fa-users"></i></span>
             <span>Friends</span>
           </span>
         </RouterLink>
-
+        <RouterLink v-if="authStore.isAdmin" to="/admin" class="navbar-item">
+          <span class="icon-text">
+            <span class="icon"><i class="fas fa-cog"></i></span>
+            <span>Admin</span>
+          </span>
+        </RouterLink>
       </div>
 
       <div class="navbar-end">
         <div class="navbar-item">
           <span class="icon-text has-text-grey-light">
-            <span class="icon"><i class="fas fa-user-circle"></i></span>
-            <span>Main</span>
+            <figure class="image is-64x128">
+              <img class="is-square" :src="authStore.currentUser?.photo" />
+            </figure>
+            <span>{{ authStore.currentUser?.name }}</span>
           </span>
         </div>
         <div class="navbar-item">
-          <button class="button is-danger is-light is-small">
+          <button class="button is-danger is-light is-small" @click="logout">
             <span class="icon"><i class="fas fa-sign-out-alt"></i></span>
-            <RouterLink to="/login">
             <span>Sign out</span>
-            </RouterLink>
           </button>
         </div>
       </div>
@@ -58,20 +59,21 @@
   </nav>
 </template>
 
-<style scoped>
-.navbar , .navbar-item, .navbar-item{
-    font-family: 'Noto';
-}
-</style>
 <script setup lang="ts">
-import {useAuthStore} from '../stores/auth'
-import {useRouter} from 'vue-router'
+import { useAuthStore } from '../stores/auth'
+import { useRouter } from 'vue-router'
 
 const authStore = useAuthStore()
 const router = useRouter()
 
-function logout(){
-    authStore.logout()
-    router.push('/login')
+function logout() {
+  authStore.logout()
+  router.push('/login')
 }
 </script>
+
+<style scoped>
+.navbar, .navbar-item {
+  font-family: 'Noto';
+}
+</style>
