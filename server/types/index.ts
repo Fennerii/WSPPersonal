4809152518d0a9d79d7dc526c8834c11516f09c7
fennerii <jonathanfennerii@gmail.com>
2026-04-27 01:43:32 -0400

    finished index.ts

--- server/types/index.ts
+++ server/types/index.ts
@@ -9,5 +9,47 @@ export type User = {
     role: UserRole
     photo: string
     password: string
-
 }
+
+export const userKeys: (keyof User)[] = [
+    "name",
+    "username",
+    "role",
+    "photo",
+    "password",
+]
+
+export type Activity = {
+    id: number
+    name: string
+}
+export const activityKeys: (keyof Activity)[] = ["name"]
+
+export type Activity = {
+    id: number
+    userId: number
+    typeId: number
+    name: string
+    date: string
+    duration: number
+    calories: number
+    notes: string
+}
+export const activityKeys: (keyof Activity)[] = [
+    "userId",
+    "typeId",
+    "name",
+    "date",
+    "duration",
+    "calories",
+    "notes",
+]
+
+export type Friend = {
+    userId: number
+    friendId: number
+}
+export const friendKeys: (keyof Friend)[] = [
+    "userId",
+    "friendId",
+]
